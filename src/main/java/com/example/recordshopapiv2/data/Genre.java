e7a88c3d41c34236d18c8f503e555554a4cabc26
Jez <jezdavisuk@gmail.com>
2024-09-14 19:19:47 +0100

    feat: populate enum Genre data model

---
+++ src/main/java/com/example/recordshopapiv2/data/Genre.java
@@ -0,0 +1,23 @@
+package com.example.recordshopapiv2.data;
+
+public enum Genre {
+    ALTERNATIVE,
+    BLUES,
+    CLASSIC,
+    COUNTRY,
+    DRUM_AND_BASS,
+    HIP_HOP,
+    HOUSE,
+    INDIE,
+    JAZZ,
+    METAL,
+    POP,
+    PUNK,
+    RAP,
+    REGGAE,
+    RHYTHM_AND_BLUES,
+    SOUL,
+    TECHNO,
+    TRANCE,
+    WORLD
+}
