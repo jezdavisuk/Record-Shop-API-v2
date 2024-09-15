d9b912c46ba647267cd1cff4bb15172630f89c16
Jez <jezdavisuk@gmail.com>
2024-09-15 17:22:12 +0100

    feat: add JPA annotations to class PriceManagerController, map to ../prices endpoint

--- src/main/java/com/example/recordshopapiv2/controller/PriceManagerController.java
+++ src/main/java/com/example/recordshopapiv2/controller/PriceManagerController.java
@@ -3,7 +3,11 @@ package com.example.recordshopapiv2.controller;
 
 import com.example.recordshopapiv2.service.PriceManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1/prices")
 public class PriceManagerController {
 
     @Autowired
