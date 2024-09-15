e285ed5112ddadd599e56ba313aca9a0611bb98b
Jez <jezdavisuk@gmail.com>
2024-09-15 16:50:00 +0100

    feat: add class ArtistManagerServiceImpl implements ArtistManagerService

---
+++ src/main/java/com/example/recordshopapiv2/service/ArtistManagerServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.recordshopapiv2.service;
+
+import com.example.recordshopapiv2.repository.ArtistManagerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArtistManagerServiceImpl implements ArtistManagerService {
+
+}
