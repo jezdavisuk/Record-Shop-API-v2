deeff6360fd8c416ddd21fcaa928d6cf57547931
Jez <jezdavisuk@gmail.com>
2024-09-15 17:10:35 +0100

    feat: add JPA annotations to class StockManagerController, map to ../stock endpoint

--- src/main/java/com/example/recordshopapiv2/controller/StockManagerController.java
+++ src/main/java/com/example/recordshopapiv2/controller/StockManagerController.java
@@ -2,7 +2,11 @@ package com.example.recordshopapiv2.controller;
 
 import com.example.recordshopapiv2.service.StockManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1/stock")
 public class StockManagerController {
 
     @Autowired
