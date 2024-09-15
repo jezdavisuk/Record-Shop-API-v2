c23464f490a601becf50f541c4e8b74e0804852c
Jez <jezdavisuk@gmail.com>
2024-09-15 17:55:47 +0100

    feat: add method getAllAlbums() to RecordManagerController, map to ../all-albums endpoint

--- src/main/java/com/example/recordshopapiv2/controller/RecordManagerController.java
+++ src/main/java/com/example/recordshopapiv2/controller/RecordManagerController.java
@@ -1,9 +1,15 @@
 package com.example.recordshopapiv2.controller;
 
+import com.example.recordshopapiv2.data.Album;
 import com.example.recordshopapiv2.service.RecordManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/records")
@@ -11,6 +17,11 @@ public class RecordManagerController {
 
     @Autowired
     RecordManagerService recordManagerService;
+
+    @GetMapping("/all-albums")
+    public ResponseEntity<List<Album>> getAllAlbums() {
+        return new ResponseEntity<>(recordManagerService.getAllAlbums(), HttpStatus.OK);
+    }
 
 
 }
