2d867420d81afa49c25097e2dba2fc587f583e86
Jez <jezdavisuk@gmail.com>
2024-09-15 17:20:56 +0100

    feat: autowire PriceManagerService to PriceManagerController

--- src/main/java/com/example/recordshopapiv2/controller/PriceManagerController.java
+++ src/main/java/com/example/recordshopapiv2/controller/PriceManagerController.java
@@ -1,4 +1,11 @@
 package com.example.recordshopapiv2.controller;
 
+
+import com.example.recordshopapiv2.service.PriceManagerService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class PriceManagerController {
+
+    @Autowired
+    PriceManagerService priceManagerService;
 }
