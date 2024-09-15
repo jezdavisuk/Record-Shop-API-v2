fa72ed471af65e4fac11485ebe1d87d0e27c73c6
Jez <jezdavisuk@gmail.com>
2024-09-15 17:39:09 +0100

    feat: add abstract method getAllAlbums() to RecordManagerService

--- src/main/java/com/example/recordshopapiv2/controller/RecordManagerController.java
+++ src/main/java/com/example/recordshopapiv2/controller/RecordManagerController.java
@@ -11,4 +11,6 @@ public class RecordManagerController {
 
     @Autowired
     RecordManagerService recordManagerService;
+
+
 }
--- src/main/java/com/example/recordshopapiv2/service/RecordManagerService.java
+++ src/main/java/com/example/recordshopapiv2/service/RecordManagerService.java
@@ -1,4 +1,9 @@
 package com.example.recordshopapiv2.service;
 
+import com.example.recordshopapiv2.data.Album;
+
+import java.util.List;
+
 public interface RecordManagerService {
+    List<Album> getAllAlbums();
 }
