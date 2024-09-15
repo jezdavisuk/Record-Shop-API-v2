08cfc0c7b718879446d2688c17c3704854ea0fcb
Jez <jezdavisuk@gmail.com>
2024-09-15 18:20:03 +0100

    feat: add test class RecordManagerRepositoryTest

---
+++ src/test/java/com/example/recordshopapiv2/repository/RecordManagerRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.example.recordshopapiv2.repository;
+
+import com.example.recordshopapiv2.data.Album;
+import com.example.recordshopapiv2.data.Genre;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class RecordManagerRepositoryTest {
+
+
+}
