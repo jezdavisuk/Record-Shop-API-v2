53ac344bcf434c95f73e66f4462e9cebcc7916ac
Jez <jezdavisuk@gmail.com>
2024-09-15 16:02:40 +0100

    feat: add JPA annotations to fields and entity relationships

--- src/main/java/com/example/recordshopapiv2/data/Stock.java
+++ src/main/java/com/example/recordshopapiv2/data/Stock.java
@@ -18,18 +18,38 @@ import java.util.Set;
 @Builder
 public class Stock {
 
+    @Id
+    @GeneratedValue
+    @Column(name="stock_id", updatable = false, nullable = false)
     Long stockId;
 
+    // relationship two
+    @ManyToOne
+    @JoinColumn(name = "album_id", referencedColumnName = "listings_in_stock", nullable = false)
     Album album;
 
+    @Column(name="format", updatable = false, nullable = false)
+    @Enumerated(EnumType.STRING)
     Medium medium;
 
+    @Column(name="quantity_in_stock")
     int stockCount;
 
+    // relationship three
+    @OneToOne(mappedBy = "stock", cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn(name="retail_price_in_pence")
     Price retailPrice;
 
+    @Column(name="last_modified")
     LocalDateTime lastModified;
 
+    // relationship four
+    @JsonIgnore
+    @ManyToMany    @JoinTable(
+            name = "signature_stock",
+            joinColumns = @JoinColumn(name = "stock_id"),
+            inverseJoinColumns = @JoinColumn(name = "artist_id")
+    )
     Set<Artist> signedBy;
 
 }
