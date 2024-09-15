c8d0069fe0e275d2d945aeca5d4768a259565f2d
Jez <jezdavisuk@gmail.com>
2024-09-15 17:18:28 +0100

    feat: autowire PriceManagerRepository to PriceManagerServiceImpl

--- src/main/java/com/example/recordshopapiv2/service/PriceManagerServiceImpl.java
+++ src/main/java/com/example/recordshopapiv2/service/PriceManagerServiceImpl.java
@@ -1,7 +1,12 @@
 package com.example.recordshopapiv2.service;
 
+import com.example.recordshopapiv2.repository.PriceManagerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PriceManagerServiceImpl implements PriceManagerService {
+
+    @Autowired
+    PriceManagerRepository priceManagerRepository;
 }
