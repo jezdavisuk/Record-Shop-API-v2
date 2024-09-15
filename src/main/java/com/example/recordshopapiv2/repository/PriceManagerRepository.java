bb94193a96af2a7637cab3d42566b5d18db2e24f
Jez <jezdavisuk@gmail.com>
2024-09-15 17:13:42 +0100

    feat: add interface PriceManagerRepository extends CrudRepository

---
+++ src/main/java/com/example/recordshopapiv2/repository/PriceManagerRepository.java
@@ -0,0 +1,9 @@
+package com.example.recordshopapiv2.repository;
+
+import com.example.recordshopapiv2.data.Price;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PriceManagerRepository extends CrudRepository<Price, Long> {
+}
