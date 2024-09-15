876853469e160d7d6e2424ff13aad5ec8c88acf4
Jez <jezdavisuk@gmail.com>
2024-09-15 16:29:49 +0100

    feat: autowire RecordManagerRepository to RecordManagerServiceImpl

--- src/main/java/com/example/recordshopapiv2/service/RecordManagerServiceImpl.java
+++ src/main/java/com/example/recordshopapiv2/service/RecordManagerServiceImpl.java
@@ -1,4 +1,10 @@
 package com.example.recordshopapiv2.service;
 
+import com.example.recordshopapiv2.repository.RecordManagerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class RecordManagerServiceImpl implements RecordManagerService {
+
+    @Autowired
+    RecordManagerRepository recordManagerRepository;
 }
