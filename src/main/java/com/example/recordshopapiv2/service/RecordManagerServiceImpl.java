f467b93c6075e7dc6e9c885b86e6a7097c025953
Jez <jezdavisuk@gmail.com>
2024-09-15 16:32:31 +0100

    feat: add @Service annotation to RecordManagerServiceImpl

--- src/main/java/com/example/recordshopapiv2/service/RecordManagerServiceImpl.java
+++ src/main/java/com/example/recordshopapiv2/service/RecordManagerServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.recordshopapiv2.service;
 
 import com.example.recordshopapiv2.repository.RecordManagerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RecordManagerServiceImpl implements RecordManagerService {
 
     @Autowired
