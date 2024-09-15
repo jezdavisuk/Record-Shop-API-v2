c8c5b7c1af79c285f48a45dde1429ac6d6f5e630
Jez <jezdavisuk@gmail.com>
2024-09-15 17:44:21 +0100

    feat: add method implementation getAllAlbums() to RecordManagerServiceImpl

--- src/main/java/com/example/recordshopapiv2/service/RecordManagerServiceImpl.java
+++ src/main/java/com/example/recordshopapiv2/service/RecordManagerServiceImpl.java
@@ -1,12 +1,23 @@
 package com.example.recordshopapiv2.service;
 
+import com.example.recordshopapiv2.data.Album;
 import com.example.recordshopapiv2.repository.RecordManagerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RecordManagerServiceImpl implements RecordManagerService {
 
     @Autowired
     RecordManagerRepository recordManagerRepository;
+
+    @Override
+    public List<Album> getAllAlbums() {
+        List<Album> albums = new ArrayList<>();
+        recordManagerRepository.findAll().forEach(albums::add);
+        return albums;
+    }
 }
