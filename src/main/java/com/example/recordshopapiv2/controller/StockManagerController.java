c28c41b6b3e1f301b5cbd261c9a5451b60aebc35
Jez <jezdavisuk@gmail.com>
2024-09-15 17:08:44 +0100

    feat: autowire StockManagerService to StockManagerController

--- src/main/java/com/example/recordshopapiv2/controller/StockManagerController.java
+++ src/main/java/com/example/recordshopapiv2/controller/StockManagerController.java
@@ -1,4 +1,10 @@
 package com.example.recordshopapiv2.controller;
 
+import com.example.recordshopapiv2.service.StockManagerService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class StockManagerController {
+
+    @Autowired
+    StockManagerService stockManagerService;
 }
