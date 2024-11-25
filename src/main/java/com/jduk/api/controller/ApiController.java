6cceab934b74962d3457f176b48b8bed6d5ae785
Jez <jezdavisuk@gmail.com>
2024-11-25 13:51:04 +0000

    feat: add POST endpoint to class ApiController creates new album record in db

--- src/main/java/com/jduk/api/controller/ApiController.java
+++ src/main/java/com/jduk/api/controller/ApiController.java
@@ -5,13 +5,12 @@ import com.jduk.api.service.ApiService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -43,6 +42,16 @@ public class ApiController {
     @GetMapping("/{id}")
     public ResponseEntity<Album> deleteAlbumByID(@PathVariable @Parameter(name = "ID", description = "Unique ID associated with a particular album of choice.") Long id){
         return new ResponseEntity<>(apiService.deleteAlbumByID(id), HttpStatus.OK);
+    }
+
+    // POST event
+    @Operation(summary = "Create new record of type Album in database.")
+    @PostMapping
+    public ResponseEntity<Album> addAlbum(@RequestBody(description = "Album to be saved to database.") Album album) {
+        Album newAlbum  = apiService.addAlbum(album);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("album", "/api/records/" + newAlbum.getAlbumId().toString());
+        return new ResponseEntity<>(newAlbum, httpHeaders, HttpStatus.CREATED);
     }
 
 
