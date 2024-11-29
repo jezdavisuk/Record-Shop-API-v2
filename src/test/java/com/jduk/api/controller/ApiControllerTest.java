1e6adad3e0881a668a66d7c079096c7725dcfbde
Jez <jezdavisuk@gmail.com>
2024-11-29 18:20:41 +0000

    test: verifies JSON response matches three instances of class Album  on three separate GET requests to /{id} endpoints

--- src/test/java/com/jduk/api/controller/ApiControllerTest.java
+++ src/test/java/com/jduk/api/controller/ApiControllerTest.java
@@ -80,6 +80,43 @@ class ApiControllerTest {
 
     }
 
+    @Test
+    @DisplayName("Mocks GET request with integer path variable, and verifies JSON response attributes match expected values on each of three separate requests to three unconstrained instances of class Album.")
+    void testGetAlbumById() throws Exception {
+        List<Album> albumList = getAlbumList();
+
+        when(mockApiServiceImpl.getAlbumById(1L)).thenReturn(albumList.get(0));
+        when(mockApiServiceImpl.getAlbumById(2L)).thenReturn(albumList.get(1));
+        when(mockApiServiceImpl.getAlbumById(3L)).thenReturn(albumList.get(2));
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/api/v1/records/1").accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.albumId").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.albumName").value("Total Life Forever"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.genre").value("INDIE"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.releaseDate").value("10-05-2010"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.ageRestricted").value(false));
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/api/v1/records/2"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.albumId").value(2L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.albumName").value("Seal"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.genre").value("RHYTHM_AND_BLUES"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.releaseDate").value("23-05-1994"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.ageRestricted").value(false));
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.get("/api/v1/records/3"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.albumId").value(3L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.albumName").value("The Blueprint"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.genre").value("RAP"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.releaseDate").value("11-09-2001"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.ageRestricted").value(true));
+    }
+
     private static List<Album> getAlbumList() {
         Album album1 = new Album(1L, "Total Life Forever", Genre.INDIE,
                 LocalDate.of(2010, Month.MAY, 10), false, null, null);
