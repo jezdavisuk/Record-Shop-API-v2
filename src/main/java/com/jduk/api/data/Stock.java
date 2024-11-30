be854e6f2c80a661a3064423ede87cc092d40957
Jez <jezdavisuk@gmail.com>
2024-11-30 16:22:04 +0000

    fix: configure unidirectional one-to-many relationship with parent Album entity, and separately a one-to-one bidirectional mapping from parent Stock entity to child Price entity

--- src/main/java/com/jduk/api/data/Stock.java
+++ src/main/java/com/jduk/api/data/Stock.java
@@ -3,9 +3,11 @@ package com.jduk.api.data;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.io.Serializable;
 import java.util.Set;
 
 import java.time.LocalDateTime;
@@ -16,14 +18,13 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "stock")
-@Builder
-public class Stock {
+@Table(name = "STOCK")
+public class Stock implements Serializable {
 
     @Id
-    @GeneratedValue
-    @Column(name="stock_id")
-    Long stockId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID", updatable = false, nullable = false)
+    private Long stockId;
 
     @Column(name="format")
     @Enumerated(EnumType.STRING)
@@ -35,18 +36,21 @@ public class Stock {
     @Column(name="last_modified")
     LocalDateTime lastModified;
 
-    @JsonIgnore
-    @ManyToOne
-    @JoinColumn(name = "artist_id", referencedColumnName = "artist_id")
-    Artist artist;
+    @OneToOne
+    @JoinColumn(name = "PRICE_ID", referencedColumnName = "ID", nullable = false)
+    private Price price;
 
-    @JsonIgnore
-    @ManyToMany
-    @JoinTable(
-            name = "stock_id_to_artist_id",
-            joinColumns = @JoinColumn(name = "stock_id"),
-            inverseJoinColumns = @JoinColumn(name = "artist_id")
-    )
-    Set<Artist> signedBy;
+//    @ManyToOne
+//    @JoinColumn(name = "artist_id", referencedColumnName = "artist_id")
+//    Artist artist;
+
+//    @JsonIgnore
+//    @ManyToMany
+//    @JoinTable(
+//            name = "stock_id_to_artist_id",
+//            joinColumns = @JoinColumn(name = "stock_id"),
+//            inverseJoinColumns = @JoinColumn(name = "artist_id")
+//    )
+//    Set<Artist> signedBy;
 
 }
